02da13a1d0d3570b1331b0c4c99f28229a66a273
dksanae09 <dekomorisanae0905@gmail.com>
2023-09-19 11:03:35 +0530

    memebers

--- src/components/playground/overview/members.tsx
+++ src/components/playground/overview/members.tsx
@@ -3,14 +3,13 @@
 import { useQuery } from "convex/react";
 import React from "react";
 import { api } from "../../../../convex/_generated/api";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { Doc } from "../../../../convex/_generated/dataModel";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Members({
   playground,
@@ -19,15 +18,16 @@ export default function Members({
 }) {
   const owner = useQuery(api.users.get, { id: playground?.owner });
   const editor = useQuery(api.users.get, { id: playground?.editor });
+
   return (
-    <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Users" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectItem value="owner">Owner: {owner?.email}</SelectItem>
-        <SelectItem value="editor">Editor: {editor?.email}</SelectItem>
-      </SelectContent>
-    </Select>
+    <Accordion type="single" collapsible>
+      <AccordionItem value="User Details">
+        <AccordionTrigger>Members</AccordionTrigger>
+        <AccordionContent className="text-primary">
+          {owner?.email}
+        </AccordionContent>
+        <AccordionContent>{editor?.email}</AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 }
