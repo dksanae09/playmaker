519740d934dd9abaa3a6d0bcd9dcd2cf5d0d2f00
dksanae09 <dekomorisanae0905@gmail.com>
2023-09-19 23:28:40 +0530

    bug fix user login

--- src/components/topbar/clerkUserProfile.tsx
+++ src/components/topbar/clerkUserProfile.tsx
@@ -1,12 +1,24 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { SignIn, SignInButton, UserButton, useUser } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { useTheme } from "next-themes";
 import React from "react";
+import { Button } from "../ui/button";
 
 export default function ClerkUserProfile() {
+  const { user } = useUser();
   const { theme } = useTheme();
+
+  if (!user)
+    return (
+      <div className="flex h-fit w-fit items-center">
+        <Button variant="ghost">
+          <SignInButton />
+        </Button>
+      </div>
+    );
+
   return (
     <div className="flex h-fit w-fit items-center">
       {theme === "dark" ? (
